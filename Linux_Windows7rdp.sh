3068fa6722875836e5c340cb0da04b015e4571cc
sanclovin2 <89048823+sanclovin2@users.noreply.github.com>
2021-08-17 17:58:15 +0700

    Update Linux_Windows7rdp.sh

--- Linux_Windows7rdp.sh
+++ Linux_Windows7rdp.sh
@@ -1,3 +1,4 @@
+
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
